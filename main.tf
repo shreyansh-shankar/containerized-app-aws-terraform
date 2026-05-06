2a3f31a5a631885257667a89c859777c28a69bbd
shreyansh-shankar <shreyanshshankar257@gmail.com>
2026-05-06 19:23:53 +0530

    updated gitignore and main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,16 @@ resource "aws_instance" "devops_demo" {
 
     security_groups = ["launch-wizard-2"]
 
+    user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install docker.io -y
+              sudo systemctl start docker
+              sudo systemctl enable docker
+
+              sudo docker run -d -p 3000:3000 -e ENV=production -e VERSION=1.0 oxfsociety/devops-demo
+              EOF
+
     tags = {
         Name = "terraform-demo"
     }
